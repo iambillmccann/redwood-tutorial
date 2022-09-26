820a995af1d436bec18e217f141715fa7b2efb2a
iambillmccann <bill.mccann@gmail.com>
2022-09-26 13:58:53 -0400

    CH6 Multiple Comments

--- web/src/components/Article/Article.test.js
+++ web/src/components/Article/Article.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from '@redwoodjs/testing'
+import { render, screen, waitFor } from '@redwoodjs/testing'
+
+import { standard } from 'src/components/CommentsCell/CommentsCell.mock'
 
 import Article from './Article'
 
@@ -17,11 +19,13 @@ describe('Article', () => {
     expect(screen.getByText(ARTICLE.body)).toBeInTheDocument()
   })
 
-  it('renders a blog post when explicity set', () => {
-    render(<Article article={ARTICLE} summary={false} />)
+  it('renders comments when displaying a full blog post', async () => {
+    const comment = standard().comments[0]
+    render(<Article article={ARTICLE} />)
 
-    expect(screen.getByText(ARTICLE.title)).toBeInTheDocument()
-    expect(screen.getByText(ARTICLE.body)).toBeInTheDocument()
+    await waitFor(() =>
+      expect(screen.getByText(comment.body)).toBeInTheDocument()
+    )
   })
 
   it('renders a summary of a blog post', () => {
@@ -33,5 +37,14 @@ describe('Article', () => {
         'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Str...'
       )
     ).toBeInTheDocument()
+  })
+
+  it('does not render comments when displaying a summary', async () => {
+    const comment = standard().comments[0]
+    render(<Article article={ARTICLE} summary={true} />)
+
+    await waitFor(() =>
+      expect(screen.queryByText(comment.body)).not.toBeInTheDocument()
+    )
   })
 })
